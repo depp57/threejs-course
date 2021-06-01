4babbc3ecb26826c1c7bed80b91b3a2605759a95
depp57 <deppdu5758@gmail.com>
2021-06-01 23:27:15 +0200

    1.06-animations

--- 1-basics/06-animations/src/main.ts
+++ 1-basics/06-animations/src/main.ts
@@ -1,8 +1,63 @@
-import './style.css'
+import './style.css';
+import * as THREE from 'three';
+import { Clock } from 'three';
+import gsap from 'gsap';
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color: 0xff0000});
+const mesh     = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+};
+
+// Camera
+const camera      = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas as HTMLCanvasElement,
+    antialias: true
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+// Clock
+// const clock = new Clock();
+
+gsap.to(mesh.position, {duration: 1, delay: 1, x: 2});
+gsap.to(mesh.position, {duration: 1, delay: 2, x: 0});
+
+
+// Animations
+const tick = () => {
+
+    // Clock
+    // const elapsedTime = clock.getElapsedTime();
+    //
+    // // Update objects
+    // mesh.rotation.y   = elapsedTime * Math.PI;
+    // camera.position.y = Math.sin(elapsedTime * Math.PI);
+    // camera.position.x = Math.cos(elapsedTime * Math.PI);
+
+    // camera.lookAt(mesh.position);
+
+    // Render
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(tick);
+};
+
+tick();
---
+++ build_course.ps1
@@ -0,0 +1,45 @@
+$course_name = $args[0]
+$temp_folder = "temp"
+
+$path_to_static_old = "$temp_folder/static"
+$path_to_static_new = "$course_name/static"
+
+$path_to_script_old = "$temp_folder/src/script.js"
+$path_to_script_new = "$course_name/src/main.ts"
+
+$path_to_css_old = "$temp_folder/src/style.css"
+$path_to_css_new = "$course_name/src/style.css"
+
+
+Write-Host "building $course_name..."
+
+
+Expand-Archive -Force ./$course_name.zip -DestinationPath ./$temp_folder
+
+
+Invoke-Expression "npm init @vitejs/app $course_name --template vanilla-ts"
+Set-Location ./$course_name
+Invoke-Expression "npm install"
+Invoke-Expression "npm install three @types/three"
+Remove-Item -LiteralPath "./src/vite-env.d.ts" -Force
+
+
+Set-Location ../
+if (Test-Path $path_to_static_old) {
+    Move-Item -Path $path_to_static_old -Destination $path_to_static_new -force
+}
+
+if (Test-Path $path_to_script_old) {
+    Move-Item -Path $path_to_script_old -Destination $path_to_script_new -force
+}
+
+if (Test-Path $path_to_css_old) {
+    Move-Item -Path $path_to_css_old -Destination $path_to_css_new -force
+}
+
+
+Remove-Item -LiteralPath "./$temp_folder" -Force -Recurse
+Remove-Item -LiteralPath "./$course_name.zip" -Force -Recurse
+
+
+Write-Host "done!"
